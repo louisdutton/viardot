e2f8aad56412725aa4559c966ac4a664ebd0c1f6
Louis Dutton <clouisdutton@gmail.com>
2021-09-29 17:54:49 +0100

    reduced test to 4 voices

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -13,37 +13,23 @@ function App() {
       Fach.Baritone,
       Fach.Bass
     ]);
-    const Q2 = new Quartet([
-      Fach.Soprano,
-      Fach.Mezzo,
-      Fach.Mezzo,
-      Fach.Contralto
-    ]);
 
     const root = 440
 
     window.onmousemove = e => {
       Q.voices[3].setFrequency((1 - e.clientY/window.innerHeight) * root)
-      Q.voices[2].setFrequency((1 - e.clientY/window.innerHeight) * root * 2)
-      Q.voices[1].setFrequency((1 - e.clientY/window.innerHeight) * root * 3)
-      Q.voices[0].setFrequency((1 - e.clientY/window.innerHeight) * root * 4)
-
-      Q2.voices[3].setFrequency((1 - e.clientY/window.innerHeight) * root * 5)
-      Q2.voices[2].setFrequency((1 - e.clientY/window.innerHeight) * root * 6)
-      Q2.voices[1].setFrequency((1 - e.clientY/window.innerHeight) * root * 7)
-      Q2.voices[0].setFrequency((1 - e.clientY/window.innerHeight) * root * 8)
+      Q.voices[2].setFrequency((1 - e.clientY/window.innerHeight) * root * 3/2)
+      Q.voices[1].setFrequency((1 - e.clientY/window.innerHeight) * root * 5/3)
+      Q.voices[0].setFrequency((1 - e.clientY/window.innerHeight) * root * 7/3)
 
       Q.voices.forEach(v => v.setIntensity(e.clientX/window.innerWidth))
-      Q2.voices.forEach(v => v.setIntensity(e.clientX/window.innerWidth))
     }
 
     window.onmousedown = e => {
       Q.voices.forEach(v => v.start())
-      Q2.voices.forEach(v => v.start())
     }
     window.onmouseup = e => {
       Q.voices.forEach(v => v.stop())
-      Q2.voices.forEach(v => v.stop())
     }
   })
 
