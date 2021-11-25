d2303395a1abf81fb25cfc8e2e043a82ddde64b3
Louis Dutton <clouisdutton@gmail.com>
2021-11-25 18:50:33 +0000

    refactored rust crate

--- src/rust/src/tract/cavity.rs
+++ src/rust/src/tract/cavity.rs
@@ -1,4 +1,4 @@
-/// Number of segments in the each cavity.
+// Number of segments in the each cavity.
 pub const ORAL_LENGTH: usize = 44;
 pub const NASAL_LENGTH: usize = 28;
 
@@ -6,14 +6,14 @@ pub struct OralCavity {
   pub k: [f32; ORAL_LENGTH],
   pub left: [f32; ORAL_LENGTH],
   pub right: [f32; ORAL_LENGTH],
-  pub junction_left: [f32; ORAL_LENGTH],
-  pub junction_right: [f32; ORAL_LENGTH],
+  pub junction_left: [f32; ORAL_LENGTH + 1],
+  pub junction_right: [f32; ORAL_LENGTH + 1],
 }
 
 pub struct NasalCavity {
   pub k: [f32; NASAL_LENGTH],
   pub left: [f32; NASAL_LENGTH],
   pub right: [f32; NASAL_LENGTH],
-  pub junction_left: [f32; NASAL_LENGTH],
-  pub junction_right: [f32; NASAL_LENGTH],
+  pub junction_left: [f32; NASAL_LENGTH + 1],
+  pub junction_right: [f32; NASAL_LENGTH + 1],
 }
