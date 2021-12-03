1438ba405436b973d062f8082f78c83ac7e86d40
Louis Dutton <clouisdutton@gmail.com>
2021-12-03 02:22:48 +0000

    converted Cavity from array to vec

--- src/rust/src/voice/tract/cavity.rs
+++ src/rust/src/voice/tract/cavity.rs
@@ -1,30 +1,24 @@
-/// Number of sections in the oral cavity.
-pub const ORAL_LENGTH: usize = 44;
-/// Number of sections in the oral cavity.
-pub const NASAL_LENGTH: usize = 28;
-
-/// The oral cavity (composed of buccal & pharyngeal cavities).
-pub struct OralCavity {
-  pub k: [f32; ORAL_LENGTH],
+/// A line of sections through which sound can propagate.
+pub struct Cavity {
+  pub k: Vec<f32>,
   /// Left-moving components at section.
-  pub left: [f32; ORAL_LENGTH],
+  pub left: Vec<f32>,
   /// Right-moving components at section.
-  pub right: [f32; ORAL_LENGTH],
+  pub right: Vec<f32>,
   /// Left-moving components at junction.
-  pub j_left: [f32; ORAL_LENGTH + 1],
+  pub j_left: Vec<f32>,
   /// Right-moving components at junction.
-  pub j_right: [f32; ORAL_LENGTH + 1],
+  pub j_right: Vec<f32>,
 }
 
-/// The nasal cavity.
-pub struct NasalCavity {
-  pub k: [f32; NASAL_LENGTH],
-  /// Left-moving components at section.
-  pub left: [f32; NASAL_LENGTH],
-  /// Right-moving components at section.
-  pub right: [f32; NASAL_LENGTH],
-  /// Left-moving components at junction.
-  pub j_left: [f32; NASAL_LENGTH + 1],
-  /// Right-moving components at junction.
-  pub j_right: [f32; NASAL_LENGTH + 1],
+impl Cavity {
+  pub fn new(N: usize) -> Cavity {
+    Cavity {
+      k: Vec::with_capacity(N),
+      left: Vec::with_capacity(N),
+      right: Vec::with_capacity(N),
+      j_left: Vec::with_capacity(N + 1),
+      j_right: Vec::with_capacity(N + 1),
+    }
+  }
 }
