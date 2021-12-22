b84167e93955d7e19ca708676eb2cb5d1adc517a
Louis Dutton <clouisdutton@gmail.com>
2021-12-22 16:10:05 +0000

    binding wasm to worklet

---
+++ test/public/pkg/belcanto_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_voice_free(a: number): void;
+export function voice_new(a: number, b: number): number;
+export function voice_process(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
