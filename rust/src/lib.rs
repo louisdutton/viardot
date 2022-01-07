86f5f8d4b8d365bc0ff18255df155d089057a9a2
Louis Dutton <clouisdutton@gmail.com>
2022-01-07 19:08:49 +0000

    removed tets

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,26 @@
+pub mod errors;
+pub mod filter;
+pub mod serialization;
+pub mod synthesis;
+pub mod theory;
 pub mod utils;
 pub mod voice;
-pub mod synthesis;
-pub mod serialization;
-pub mod theory;
 pub mod waveform;
-pub mod filter;
-pub mod errors;
-// use wasm_bindgen::prelude::*;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
--- rust/src/voice/mod.rs
+++ rust/src/voice/mod.rs
@@ -10,31 +10,38 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Voice {
-  glottis: Glottis,
-  tract: Tract,
-  // sample_rate: usize,
-  // block_size: usize,
+    glottis: Glottis,
+    tract: Tract,
+    // sample_rate: usize,
+    // block_size: usize,
 }
 
 #[wasm_bindgen]
 impl Voice {
-  pub fn new(sample_rate: usize, block_size: usize) -> Voice {
-    set_panic_hook();
-    Voice {
-      glottis: Glottis::new(sample_rate, block_size),
-      tract: Tract::new(sample_rate, block_size),
-      // sample_rate,
-      // block_size,
+    #[wasm_bindgen(constructor)]
+    pub fn new(sample_rate: usize, block_size: usize) -> Voice {
+        set_panic_hook();
+        Voice {
+            glottis: Glottis::new(sample_rate, block_size),
+            tract: Tract::new(sample_rate, block_size),
+            // sample_rate,
+            // block_size,
+        }
     }
-  }
 
-  pub fn process(&mut self, time: f64) -> Vec<f64> {
-    // create output buffer
-    // let noise = [0.0; 128]; // TODO actually generate noise
-    let out = self.glottis.process(time);
-    // Filter source via the vocal stract
-    // out = self.tract.process(&out, &noise);
+    #[wasm_bindgen]
+    pub fn process(&mut self, time: f64) -> Vec<f64> {
+        // create output buffer
+        // let noise = [0.0; 128]; // TODO actually generate noise
+        let out = self.glottis.process(time);
+        // Filter source via the vocal stract
+        // out = self.tract.process(&out, &noise);
 
-    return out;
-  }
+        return out;
+    }
+
+    #[wasm_bindgen]
+    pub fn test() -> bool {
+        true
+    }
 }
